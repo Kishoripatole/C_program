0b5651c5d229ca02231e4c6b37bdb691409c7672
Kishoripatole <Kishoripatole2006@gmail.com>
2026-04-16 19:46:22 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,23 @@
+// #include<stdio.h>   
+// #include <string.h>      
+// int main(){     
+
+// char ch[11]={ 't', 'p', 'o', 'i', 'n', 't', 't', 'e', 'c', 'h', '\0'};      
+// char ch2[11]="tpointtech";      
+      
+   
+// printf("Char Array Value is: %s\n", ch);      
+// printf("String Literal Value is: %s\n", ch2);      
+//  return 0;      
+// }      
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[]="Hell C programming";
+
+    printf("%c\n",str[5]);
+    printf("%c\n",str[3]);
+    printf("%c\n",str[7]);
+}
