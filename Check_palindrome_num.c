7a34a5010492ad804ac1c41da91565c602b359d6
Kishoripatole <Kishoripatole2006@gmail.com>
2026-05-01 13:32:24 +0530

    if reversed no. is equal to original no. ;

--- Check_palindrome_num.c
+++ Check_palindrome_num.c
@@ -1,5 +1,29 @@
 #include<stdio.h>
 int palindrome();
+int main()
+{
+  palindrome();
+  return 0;
+}
+int palindrome()
+{
+  int n,n1,n2,n3,reverse;
+  printf("Enter an integer:");
+  scanf("%d",&n);
+ n1=n/100;
+ n2=(n/10)%10;
+ n3=(n%100)%10;
+ reverse=(n1*100)+(n2*10)+n1;
+ if(reverse==n)
+{
+   printf("Number is palindrome.");
+}
+else
+{
+   printf("Number is not palindrome.");
+}
+return 0;
+}
 
 
 
