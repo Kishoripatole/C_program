fcd612f1c79e57c3744d23f32585e8d072b397cd
Kishoripatole <Kishoripatole2006@gmail.com>
2026-05-04 12:43:14 +0530

    Add files via upload
    Colorful world

---
+++ color_format.c
@@ -0,0 +1,39 @@
+// #include <stdio.h>
+
+// int main() {
+//     printf("\033[31mThis is Red Text\033[0m\n");
+//     printf("\033[32mThis is Green Text\033[0m\n");
+//     printf("\033[34mThis is Blue Text\033[0m\n");
+//     printf("\033[33mThis is Yellow Text\033[0m\n");
+
+//     return 0;
+// }
+
+
+
+
+
+#include<stdio.h>
+int main(){
+      printf("\033[34;41m Jay hind\033  [0m\n");
+      printf("\033[34;41m jay hind\033  [0m\n");
+      printf("\033[34;41m Jay hind\033  [0m\n");
+      printf("\033[34;41m Jay hind\033  [0m\n");
+      printf("\033[34;47m Jay hind\033  [0m\n");
+      printf("\033[34;47m Jay hind\033  [0m\n");
+      printf("\033[34;47m Jay hind\033  [0m\n");
+      printf("\033[34;47m Jay hind\033  [0m\n");
+      printf("\033[34;42m Jay hind\033  [0m\n");
+      printf("\033[34;42m Jay hind\033  [0m\n");
+      printf("\033[34;42m Jay hind\033  [0m\n");
+      printf("\033[34;42m Jay hind\033  [0m\n");
+
+}
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\033[33m Hello in the world of c programming\033[0m");
+// }
