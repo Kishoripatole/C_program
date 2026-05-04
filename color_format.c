d17f4e842b399dcbedc8e1facdae4535eff54b7a
Kishoripatole <Kishoripatole2006@gmail.com>
2026-05-04 07:25:37 +0000

    Colorful world

--- color_format.c
+++ color_format.c
@@ -15,18 +15,18 @@
 
 #include<stdio.h>
 int main(){
-      printf("\033[34;41m Jay hind\033  [0m\n");
-      printf("\033[34;41m jay hind\033  [0m\n");
-      printf("\033[34;41m Jay hind\033  [0m\n");
-      printf("\033[34;41m Jay hind\033  [0m\n");
-      printf("\033[34;47m Jay hind\033  [0m\n");
-      printf("\033[34;47m Jay hind\033  [0m\n");
-      printf("\033[34;47m Jay hind\033  [0m\n");
-      printf("\033[34;47m Jay hind\033  [0m\n");
-      printf("\033[34;42m Jay hind\033  [0m\n");
-      printf("\033[34;42m Jay hind\033  [0m\n");
-      printf("\033[34;42m Jay hind\033  [0m\n");
-      printf("\033[34;42m Jay hind\033  [0m\n");
+      printf("\033[34;41m Jay hind  \033  [0m\n");
+      printf("\033[34;41m jay hind  \033  [0m\n");
+      printf("\033[34;41m Jay hind  \033  [0m\n");
+      printf("\033[34;41m Jay hind  \033  [0m\n");
+      printf("\033[34;47m Jay hind  \033  [0m\n");
+      printf("\033[34;47m Jay hind  \033  [0m\n");
+      printf("\033[34;47m Jay hind  \033  [0m\n");
+      printf("\033[34;47m Jay hind  \033  [0m\n");
+      printf("\033[34;42m Jay hind  \033  [0m\n");
+      printf("\033[34;42m Jay hind  \033  [0m\n");
+      printf("\033[34;42m Jay hind  \033  [0m\n");
+      printf("\033[34;42m Jay hind  \033  [0m\n");
 
 }
 
@@ -34,6 +34,6 @@ int main(){
 
 // #include<stdio.h>
 // int main()
-// {
+//{
 //     printf("\033[33m Hello in the world of c programming\033[0m");
 // }
